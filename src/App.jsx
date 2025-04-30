8c4e8e7298048798c1be1b96c5559a301afe3e85
Vidita Singh <vidita1812@gmail.com>
2025-04-30 15:02:34 +0530

    add conversion functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { useState } from 'react'
 import './App.css'
+import {InputBox} from './components '
+import {useCurrencyInfo} from './hooks/useCurrencyInfo'
 
 function App() {
+
+  const [amount,setAmount] = useState(0)
+  const [from,setFrom] = useState("inr")
+  const [to,setTo] = useState("usd")
+  const[convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () =>{
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () => {setConvertedAmount(amount*currencyInfo[to])}
 
   return (
     <>
